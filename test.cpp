01d141d4935e7634aee3d9e4eab6efdb3d5bc11d
griyn <godlights@outlook.com>
2018-05-31 23:16:36 +0800

    update date

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 
 int main() {
 	std::cout << "Hello World" << std::endl;
-	std::cout << "update on git" << std::endl;
+	std::cout << "update on git 2018.5.31" << std::endl;
+	int i = 1;
+	std::cout << i << std::endl;
 	return 0;
 }
